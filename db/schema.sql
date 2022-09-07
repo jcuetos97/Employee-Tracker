cb16fc70e82fa7dad23344dd19a4fabad3503b5d
jcuetos97 <105081149+jcuetos97@users.noreply.github.com>
2022-09-07 18:43:51 -0500

    schema.sql & seeds.sql created

--- db/schema.sql
+++ db/schema.sql
@@ -3,25 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments {
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE departments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
-};
+);
 
-CREATE TABLE roles {
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE roles (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT, 
-    FOREIGN KEY (department_id) REFERENCES department(id)
-};
+    FOREIGN KEY (department_id) REFERENCES departments(id)
+);
 
-CREATE TABLE employees {
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE employees (
+    id INT PRIMARY KEY AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT, 
     manager_id INT,
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES role(id) ON DELETE SET NULL,
-};
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id) ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,30 +1,30 @@
 
-INSERT INTO department (name)
+INSERT INTO departments (name)
 VALUES 
 ('Sales'),
 ('Engineering'),
 ('Finance'),
 ('Legal');
 
-INSERT INTO role (title, salary, department_id)
+INSERT INTO roles (title, salary, department_id)
 VALUES
 ('Sales Lead', 100000, 1),
 ('Salesperson', 80000, 1),
-('Accountant', 10000, 2), 
-('Finanical Analyst', 150000, 2),
-('Marketing Coordindator', 70000, 3), 
-('Sales Lead', 90000, 3),
-('Project Manager', 100000, 4),
-('Operations Manager', 90000, 4);
+('Lead Engineer', 150000, 2), 
+('Software Engineer', 120000, 2),
+('Account Manager', 160000, 3), 
+('Accountant', 1250000, 3),
+('Legal Team Lead', 250000, 4),
+('Lawyer', 190000, 4);
 
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES 
-('Mark', 'Miller', 2, null),
-('Devin', 'Anderson', 1, 1),
-('Mary', 'Brown', 4, null),
-('Ashley', 'Jones', 3, 3),
-('Tyler', 'Moore', 6, null),
-('Ana', 'Sanchez', 5, 5),
-('Lewis', 'Allen', 7, null),
-('Katherine', 'Green', 8, 7);
+('John', 'Doe', 1, null),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, null),
+('Kevin', 'Tupik', 4, 3),
+('Kunal', 'Singh', 5, null),
+('Malia', 'Brown', 6, 5),
+('Sarah', 'Lourd', 7, null),
+('Tom', 'Allen', 8, 7);
